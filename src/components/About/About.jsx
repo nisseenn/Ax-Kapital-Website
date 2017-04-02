4a1fc9baf00d17ab85efcee9cdd0fedbfaf6a7b0
Magnus Drangevåg <magnusdr@gmail.com>
2017-04-02 21:52:13 +0200

    Added coffecup

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,8 +7,8 @@ import coffe from "./img/coffe.png"
 var About = React.createClass({
   render: function() {
     return <div className={css.About}>
-      <h1>Om engleinvestor</h1>
       <img src={coffe} />
+      <h1>Om engleinvestor</h1>
       <p>Med Engleinvestor kan du som investor enkelt få tilgang til selskapspresentasjoner online. I samarbeid med Seed Forum, Aksjeforum og Gründerforum sørger vi for at du får seriøse og velinformerende selskapspresentasjoner. I tillegg sørger vi for at selskapene er godt forberedt gjennom onlinekurset til Seed Forum, slik at du som investor for en best mulig pitch. Ved interesse blir du som investor invitert til en lukket gruppe på maksimalt 150.</p>
     </div>
   }
