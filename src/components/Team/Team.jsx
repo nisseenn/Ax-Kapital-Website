8187704d73ec604f65799b8cae873d89b6091041
nisseenn <alex_nissen@hotmail.com>
2017-04-02 18:40:53 +0200

    added picture

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -6,7 +6,12 @@ const css = require('./css/Team.css')
 var Team = React.createClass({
   render: function() {
     return <div className={css.Team}>
-      <h1>Team</h1>
+      <h1>Teamet</h1>
+        <div className={css.row}>
+          <div className={css.pictures}>
+
+        </div>
+      </div>
     </div>
   }
 });
