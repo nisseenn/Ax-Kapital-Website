4177ae72d366b6b0f30a2c833dbcf451559799a5
Magnus Drangevåg <magnusdr@gmail.com>
2017-04-02 14:07:03 +0200

    Parallax effect now working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,18 @@
 const React = require('react');
 import { connect } from 'react-redux'
+import { Parallax } from 'react-parallax';
+import bgImage from './img/bg.jpg'
 
 const css = require('./css/Header.css')
 
 var Header = React.createClass({
-  onscroll:function(){
-
-  },
   render: function() {
-    var top = window.addEventListener('scroll', this.handleScroll).pageYOffset;
-    var backgroundStyle = {
-      backgroundPositionY: top
-    };
-    console.log(top)
-    return <div className={css.Header} style={backgroundStyle}>
-      <h1>Engleinvestor</h1>
-      <p> Engleinvestor er en....</p>
-    </div>
-
+    return <Parallax bgImage={bgImage} strength={400}>
+      <div className={css.Header}>
+        <h1>Engleinvestor</h1>
+        <p> Engleinvestor er en....</p>
+      </div>
+    </Parallax>
   }
 });
 
