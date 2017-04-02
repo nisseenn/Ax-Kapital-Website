92e13330f181a453df5b3b90b84318aedb242cae
Magnus Drangevåg <magnusdr@gmail.com>
2017-04-02 17:43:20 +0200

    Made the Guide component

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'], query: { presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react'] } },
       { test: /\.jsx$/, exclude: /node_modules/, loader: ['babel-loader'], query: { presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react'] } },
       { test: /\.css$/, loader: "style-loader!css-loader?localIdentName=[name]__[local]___[hash:base64:5]" },
-      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.png$/, loader: "url-loader?limit=10000000" },
       { test: /\.jpg$/, loader: "url-loader?limit=10000000" },
       { test: /\.otf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
     ]
