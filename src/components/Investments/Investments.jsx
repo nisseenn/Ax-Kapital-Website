4fddebdc1d1080ee1aa0816580556d8f5ff503a3
nisseenn <alex_nissen@hotmail.com>
2017-04-02 20:25:58 +0200

    added investments

--- src/components/Investments/Investments.jsx
+++ src/components/Investments/Investments.jsx
@@ -6,7 +6,12 @@ const css = require('./css/Investments.css')
 var Investments = React.createClass({
   render: function() {
     return <div className={css.Investments}>
-      <h1>Investments</h1>
+      <h1>Invested capital so far</h1>
+        <div className={css.container}>
+          <div className={css.amountInvested}>
+            120
+          </div>
+        </div>
     </div>
   }
 });
