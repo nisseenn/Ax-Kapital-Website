12cdb957369d03969cebac4586f7a79bd50f9692
nisseenn <alex_nissen@hotmail.com>
2017-04-02 13:44:43 +0200

    implementing parallax effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,20 @@ import { connect } from 'react-redux'
 const css = require('./css/Header.css')
 
 var Header = React.createClass({
+  onscroll:function(){
+
+  },
   render: function() {
-    return <div className={css.Header}>
+    var top = window.addEventListener('scroll', this.handleScroll).pageYOffset;
+    var backgroundStyle = {
+      backgroundPositionY: top
+    };
+    console.log(top)
+    return <div className={css.Header} style={backgroundStyle}>
       <h1>Engleinvestor</h1>
+      <p> Engleinvestor er en....</p>
     </div>
+
   }
 });
 
