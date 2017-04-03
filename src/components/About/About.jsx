b708b21ac956f6c5b22c2d7c6af9eb87b05241ad
nisseenn <alex_nissen@hotmail.com>
2017-04-03 09:27:28 +0200

    added missing header for about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,6 +8,7 @@ var About = React.createClass({
   render: function() {
     return <div className={css.About}>
       <img src={coffe} />
+      <h1>Om engleinvestor</h1>
       <p>Er du interessert i oppstartsselskaper? Med Engleinvestor kan du enkelt få innsikt i de forskjellige selskapene gjennom presentasjoner på et eget nettsted. I samarbeid med Seed Forum, Aksjeforum og Gründerforum sørger vi for at du får seriøse og velinformerende selskapspresentasjoner. I tillegg sørger vi for at selskapene er godt forberedt gjennom onlinekurset til Seed Forum, slik at du som investor for en best mulig pitch. Ved interesse blir du som investor invitert til en lukket gruppe på maksimalt 150.</p>
     </div>
   }
