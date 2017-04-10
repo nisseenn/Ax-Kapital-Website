8ea35be0b88f0925577265df9b15704dbe9d67fd
Magnus Drangevåg <magnusdr@gmail.com>
2017-04-10 07:01:51 +0200

    Changed webpack config and babel config to adapt to the newer 2.x version of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,11 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'], query: { presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react'] } },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: ['babel-loader'], query: { presets: ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react'] } },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: "style-loader!css-loader?localIdentName=[name]__[local]___[hash:base64:5]" },
       { test: /\.png$/, loader: "url-loader?limit=10000000" },
       { test: /\.jpg$/, loader: "url-loader?limit=10000000" },
-      { test: /\.otf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      { test: /\.otf$/, loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
     ]
   }
 };
