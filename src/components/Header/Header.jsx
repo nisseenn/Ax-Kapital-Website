2a85e2a828ef5ca4145543a3f5e323cf888e23d8
nisseenn <alex_nissen@hotmail.com>
2017-04-11 19:06:26 +0200

    added buttons for team linkedin, tried to implement the JumpJS, but failed.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,22 +3,20 @@ const React = require('react');
 import { Parallax } from 'react-parallax'
 import bgImage from './img/bg.jpg'
 import jump from 'jump.js'
-import Info from '../About/About.jsx'
+import About from '../About/About.jsx'
 const css = require('./css/Header.css')
-const node = document.querySelector('.Info')
 
 var Header = React.createClass({
   scroll: function() {
-    jump(node)
+    jump(<About />);
   },
   render: function() {
     return <Parallax bgImage={bgImage} strength={400}>
       <div className={css.Header}>
         <h1>Engleinvestor</h1>
         <h2>Der de gode selskapene møter nødvendig kapital </h2>
-          <button className={css.readMore} onClick={scroll}>Les mer</button>
+          <button className={css.readMore} onClick={this.scroll}>Les mer</button>
           <button className={css.contactUs}>Kontakt oss</button>
-        <div className={css.Info} />
       </div>
 
     </Parallax>
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,6 +1,5 @@
 const React = require('react');
 
-
 const css = require('./css/Team.css')
 import PictureOne from './img/1.png'
 import PictureTwo from './img/2.png'
@@ -8,28 +7,31 @@ import PictureThree from './img/3.png'
 import PictureFour from './img/4.jpg'
 
 var Team = React.createClass({
+  toSite: function() {
+      window.open('https://www.linkedin.com/in/gleblevin/');
+    },
   render: function() {
     return <div className={css.Team}>
       <h1>Teamet</h1>
       <div className={css.pictures}>
         <img className={css.picture1} src={PictureOne} />
         <h2>Gleb Levin</h2>
-        <p>quote of life</p>
+        <button onclick={this.toSite}>LinkedInLogo</button>
       </div>
     <div className={css.pictures}>
       <img className={css.picture2} src={PictureTwo} />
       <h2>Berg Moe</h2>
-      <p>LinkedIn here</p>
+      <button>Linkdinlogo</button>
     </div>
   <div className={css.pictures}>
     <img className={css.picture3} src={PictureThree} />
     <h2>Obaid Salem</h2>
-    <p>LinkedIn here</p>
+    <a>LinkedIn logo here</a>
   </div>
   <div className={css.pictures}>
     <img className={css.picture4} src={PictureFour} />
     <h2>Nicolai Thorgersen</h2>
-    <p>LinkedIn here</p>
+    <a>LinkedIn logo here</a>
   </div>
   </div>
   }
