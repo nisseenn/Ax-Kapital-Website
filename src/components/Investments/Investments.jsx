db32d7f02d65e2cc473c94d76b41aa740cbe8eba
Alexander <alex_nissen@hotmail.com>
2017-05-17 19:36:33 +0200

    added a new favicon

--- src/components/Investments/Investments.jsx
+++ src/components/Investments/Investments.jsx
@@ -11,7 +11,7 @@ var Investments = React.createClass({
   render: function() {
     return <Parallax bgImage={bgImage} strength={300}>
     <div className={css.Investments}>
-      <h1>Der de gode selskapene møter kompetent kapital</h1>
+      <h1>Der de gode selskapene møter nødvendig kapital</h1>
         {/*<span className="countUp">
           <CountUp start={0} end={0} duration={5} useEasing={true} />
         </span>
